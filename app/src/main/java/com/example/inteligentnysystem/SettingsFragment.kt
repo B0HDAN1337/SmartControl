d4b80eb4d6c349937385c108f026f39b233716c0
ghost <machuliak1337@gmail.com>
2025-04-11 18:40:51 +0200

    Add function to search bluetooth devices in application

--- app/src/main/java/com/example/inteligentnysystem/SettingsFragment.kt
+++ app/src/main/java/com/example/inteligentnysystem/SettingsFragment.kt
@@ -1,33 +1,27 @@
 package com.example.inteligentnysystem
 
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SettingsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SettingsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var deviceListItem: ListView
+    private lateinit var deviceListAdapter: ArrayAdapter<String>
+    private val deviceNames = ArrayList<String>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,23 +31,65 @@ class SettingsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Settings.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SettingsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    @SuppressLint("MissingPermission")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+         val bluetoothManager: BluetoothManager = requireContext().getSystemService(BluetoothManager::class.java)
+         val bluetoothAdapter: BluetoothAdapter = bluetoothManager.adapter
+         val buttonConnect: Button = view.findViewById(R.id.buttonConntect)
+
+        deviceListItem = view.findViewById(R.id.searched_devices)
+        deviceListAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, deviceNames)
+        deviceListItem.adapter = deviceListAdapter
+
+
+        //Enable Bluetooth on click
+        buttonConnect.setOnClickListener{
+            if(!bluetoothAdapter.isEnabled)
+            {
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startActivity(enableBtIntent);
+            }
+            else // if bluetooth enabled, search devices
+            {
+                 bluetoothAdapter.startDiscovery()
+            }
+        }
+    }
+
+    //Search bluetooth devices
+    private val receiver = object : BroadcastReceiver() {
+        @SuppressLint("MissingPermission")
+        override fun onReceive(context: Context, intent: Intent)
+        {
+            val action: String = intent.action!!
+            if (action == BluetoothDevice.ACTION_FOUND) {
+                val device: BluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)!!
+                val deviceName = device.name
+                val deviceAddress = device.address
+
+                //add device name and mac address to ListView
+                val displayDevices = "$deviceName\n$deviceAddress"
+
+                if(!deviceNames.contains(displayDevices))
+                {
+                    deviceNames.add(displayDevices)
+                    deviceListAdapter.notifyDataSetChanged()
                 }
             }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        requireActivity().registerReceiver(receiver, filter)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        requireActivity().unregisterReceiver(receiver)
     }
 }
+
