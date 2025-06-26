b1491899468e2ef012083549bb9f1c413e9b9400
B0HDAN1337 <machuliak1337@gmail.com>
2025-06-26 22:37:43 +0200

    generated the initial webapi project

---
+++ InteligentServer/Program.cs
@@ -0,0 +1,32 @@
+namespace InteligentServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddAuthorization();
+            builder.Services.AddControllers();
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
